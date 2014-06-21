2ca77ad1bda30d3e544fd1911bffa9fce09d6d45
Ricardo Franco <ricardo.krieg@gmail.com>
2014-06-20 22:47:33 -0300

    planos

--- PortalDirceu/inc/planos.php
+++ PortalDirceu/inc/planos.php
@@ -138,37 +138,45 @@ function portaldirceu_planos_delete($plan_id) {
 }
 
 
+function quotescollection_bulkdelete($plan_ids) {
+    if (!$plan_ids) return "Nada feito!";
+
+    global $wpdb;
+
+    $sql = "DELETE FROM ". portaldirceu_planos_tablename() ."WHERE plan_id IN (".implode(', ', $plan_ids).")";
+
+    $wpdb->query($sql);
+
+    return "Plano(s) excluído(s)";
+}
+
+
 function portaldirceu_planos_management() {
-    $display = $msg = $quotes_list = $alternate = "";
+    $display = $msg = $plans_list = $alternate = "";
 
     if (isset($_REQUEST['submit'])) {
         if($_REQUEST['submit'] == "Adicionar Plano") {
             extract($_REQUEST);
-            $msg = portaldirceu_planos_add($quote, $author, $source);
+            $msg = portaldirceu_planos_add($name, $price, $color, $free_wifi);
         }
         else if($_REQUEST['submit'] == "Salvar") {
             extract($_REQUEST);
-            $msg = portaldirceu_planos_edit($quote_id, $quote, $author, $source);
+            $msg = portaldirceu_planos_edit($plan_id, $name, $price, $color, $free_wifi);
         }
     } else if (isset($_REQUEST['action'])) {
         if ($_REQUEST['action'] == 'edit') {
             $display .= "<div class=\"wrap\">\n<h2>Quotes Collection &raquo; ".__('Edit quote', 'quotes-collection')."</h2>";
-            $display .=  quotescollection_editform($_REQUEST['id']);
+            $display .=  portaldirceu_planos_editform($_REQUEST['id']);
             $display .= "</div>";
+
             echo $display;
             return;
-        } else if ($_REQUEST['action'] == 'del') {
-            $msg = quotescollection_deletequote($_REQUEST['id']);
+        } else if ($_REQUEST['action'] == 'delplan') {
+            $msg = portaldirceu_planos_delete($_REQUEST['id']);
         }
     } else if (isset($_REQUEST['bulkactionsubmit']))  {
         if ($_REQUEST['bulkaction'] == 'delete')
-            $msg = quotescollection_bulkdelete($_REQUEST['bulkcheck']);
-        if ($_REQUEST['bulkaction'] == 'make_public') {
-            $msg = quotescollection_changevisibility($_REQUEST['bulkcheck'], 'yes');
-        }
-        if ($_REQUEST['bulkaction'] == 'keep_private') {
-            $msg = quotescollection_changevisibility($_REQUEST['bulkcheck'], 'no');
-        }
+            $msg = portaldirceu_planos_bulkdelete($_REQUEST['bulkcheck']);
     }
 
     $display .= "<div class=\"wrap\">";
@@ -176,17 +184,17 @@ function portaldirceu_planos_management() {
     if ($msg)
         $display .= "<div id=\"message\" class=\"updated fade\"><p>{$msg}</p></div>";
 
-    $display .= "<h2>Quotes Collection <a href=\"#addnew\" class=\"add-new-h2\">".__('Add new quote', 'quotes-collection')."</a></h2>";
+    $display .= "<h2>Quotes Collection <a href=\"#addnew\" class=\"add-new-h2\">Adicionar novo Plano</a></h2>";
 
-    $num_quotes = quotescollection_count();
+    $num_plans = portaldirceu_planos_count();
 
-    if (!$num_quotes) {
-        $display .= "<p>".__('No quotes in the database', 'quotes-collection')."</p>";
+    if (!$num_plans) {
+        $display .= "<p>Nenhum Plano encontrado</p>";
 
         $display .= "</div>";
 
-        $display .= "<div id=\"addnew\" class=\"wrap\">\n<h2>".__('Add new quote', 'quotes-collection')."</h2>";
-        $display .= quotescollection_editform();
+        $display .= "<div id=\"addnew\" class=\"wrap\">\n<h2>Adicionar novo Plano</h2>";
+        $display .= portaldirceu_planos_editform();
         $display .= "</div>";
 
         echo $display;
@@ -195,32 +203,31 @@ function portaldirceu_planos_management() {
 
     global $wpdb;
 
-    $sql = "SELECT quote_id, quote, author, source, tags, public
-        FROM " . $wpdb->prefix . "quotescollection";
+    $sql = "SELECT plan_id, name, price, color, free_wifi FROM " . portaldirceu_planos_tablename();
 
     $option_selected = array (
-        'quote_id' => '',
-        'quote' => '',
-        'author' => '',
-        'source' => '',
-        'time_added' => '',
-        'time_updated' => '',
-        'public' => '',
+        'plan_id' => '',
+        'name' => '',
+        'price' => '',
+        'color' => '',
+        'free_wifi' => '',
+        'created_at' => '',
+        'updated_at' => '',
         'ASC' => '',
         'DESC' => '',
     );
-    if(isset($_REQUEST['orderby'])) {
+    if (isset($_REQUEST['orderby'])) {
         $sql .= " ORDER BY " . $_REQUEST['orderby'] . " " . $_REQUEST['order'];
         $option_selected[$_REQUEST['orderby']] = " selected=\"selected\"";
         $option_selected[$_REQUEST['order']] = " selected=\"selected\"";
     }
     else {
-        $sql .= " ORDER BY quote_id ASC";
-        $option_selected['quote_id'] = " selected=\"selected\"";
+        $sql .= " ORDER BY plan_id ASC";
+        $option_selected['plan_id'] = " selected=\"selected\"";
         $option_selected['ASC'] = " selected=\"selected\"";
     }
 
-    if(isset($_REQUEST['paged']) && $_REQUEST['paged'] && is_numeric($_REQUEST['paged']))
+    if (isset($_REQUEST['paged']) && $_REQUEST['paged'] && is_numeric($_REQUEST['paged']))
         $paged = $_REQUEST['paged'];
     else
         $paged = 1;
@@ -228,77 +235,53 @@ function portaldirceu_planos_management() {
     $limit_per_page = 20;
 
 
+    $total_pages = ceil($num_plans / $limit_per_page);
 
-    $total_pages = ceil($num_quotes / $limit_per_page);
 
+    if ($paged > $total_pages) $paged = $total_pages;
 
-    if($paged > $total_pages) $paged = $total_pages;
-
-    $admin_url = get_bloginfo('wpurl'). "/wp-admin/admin.php?page=quotes-collection";
-    if(isset($_REQUEST['orderby']))
+    $admin_url = get_bloginfo('wpurl'). "/wp-admin/admin.php?page=planos";
+    if (isset($_REQUEST['orderby']))
         $admin_url .= "&orderby=".$_REQUEST['orderby']."&order=".$_REQUEST['order'];
 
-    $page_nav = quotescollection_pagenav($total_pages, $paged, 2, 'paged', $admin_url);
+    $page_nav = portaldirceu_planos_pagenav($total_pages, $paged, 2, 'paged', $admin_url);
 
     $start = ($paged - 1) * $limit_per_page;
 
     $sql .= " LIMIT {$start}, {$limit_per_page}";
 
-    // Get all the quotes from the database
-    $quotes = $wpdb->get_results($sql);
+    $plans = $wpdb->get_results($sql);
 
-    foreach($quotes as $quote_data) {
-        if($alternate) $alternate = "";
+    foreach ($plans as $plan_data) {
+        if ($alternate) $alternate = "";
         else $alternate = " class=\"alternate\"";
-        $quotes_list .= "<tr{$alternate}>";
-        $quotes_list .= "<th scope=\"row\" class=\"check-column\"><input type=\"checkbox\" name=\"bulkcheck[]\" value=\"".$quote_data->quote_id."\" /></th>";
-        $quotes_list .= "<td>" . $quote_data->quote_id . "</td>";
-        $quotes_list .= "<td>";
-        $quotes_list .= wptexturize(nl2br(make_clickable($quote_data->quote)));
-        $quotes_list .= "<div class=\"row-actions\"><span class=\"edit\"><a href=\"{$admin_url}&action=editquote&amp;id=".$quote_data->quote_id."\" class=\"edit\">".__('Edit', 'quotes-collection')."</a></span> | <span class=\"trash\"><a href=\"{$admin_url}&action=delquote&amp;id=".$quote_data->quote_id."\" onclick=\"return confirm( '".__('Are you sure you want to delete this quote?', 'quotes-collection')."');\" class=\"delete\">".__('Delete', 'quotes-collection')."</a></span></div>";
-        $quotes_list .= "</td>";
-        $quotes_list .= "<td>" . make_clickable($quote_data->author);
-        if($quote_data->author && $quote_data->source)
-            $quotes_list .= " / ";
-        $quotes_list .= make_clickable($quote_data->source) ."</td>";
-        $quotes_list .= "<td>" . implode(', ', explode(',', $quote_data->tags)) . "</td>";
-        if($quote_data->public == 'no') $public = __('No', 'quotes-collection');
-        else $public = __('Yes', 'quotes-collection');
-        $quotes_list .= "<td>" . $public  ."</td>";
-        $quotes_list .= "</tr>";
+
+        $plans_list .= "<tr{$alternate}>";
+        $plans_list .= "<th scope=\"row\" class=\"check-column\"><input type=\"checkbox\" name=\"bulkcheck[]\" value=\"".$plan_data->plan_id."\" /></th>";
+        $plans_list .= "<td>" . $plan_data->plan_id . "</td>";
+        $plans_list .= "<td>";
+        $plans_list .= wptexturize(nl2br(make_clickable($plan_data->name)));
+        $plans_list .= "<div class=\"row-actions\"><span class=\"edit\"><a href=\"{$admin_url}&action=editquote&amp;id=".$plan_data->plan_id."\" class=\"edit\">Editar</a></span> | <span class=\"trash\"><a href=\"{$admin_url}&action=delplan&amp;id=".$plan_data->plan_id."\" onclick=\"return confirm( 'Você tem certeza de que quer excluir este Plano?');\" class=\"delete\">Excluir</a></span></div>";
+        $plans_list .= "</td>";
+        $plans_list .= "<td>". make_clickable($plan_data->price) ."</td>";
+        $plans_list .= "</tr>";
     }
 
-    if($quotes_list) {
-        $quotes_count = quotescollection_count();
+    if ($plans_list) {
+        $plans_count = portaldirceu_planos_count();
 
-        $display .= "<form id=\"quotescollection\" method=\"post\" action=\"".get_bloginfo('wpurl')."/wp-admin/admin.php?page=quotes-collection\">";
+        $display .= "<form id=\"portaldirceu_planos\" method=\"post\" action=\"".get_bloginfo('wpurl')."/wp-admin/admin.php?page=planos\">";
         $display .= "<div class=\"tablenav\">";
         $display .= "<div class=\"alignleft actions\">";
         $display .= "<select name=\"bulkaction\">";
         $display .=     "<option value=\"0\">".__('Bulk Actions')."</option>";
-        $display .=     "<option value=\"delete\">".__('Delete', 'quotes-collection')."</option>";
-        $display .=     "<option value=\"make_public\">".__('Make public', 'quotes-collection')."</option>";
-        $display .=     "<option value=\"keep_private\">".__('Keep private', 'quotes-collection')."</option>";
+        $display .=     "<option value=\"delete\">Excluir</option>";
         $display .= "</select>";
-        $display .= "<input type=\"submit\" name=\"bulkactionsubmit\" value=\"".__('Apply', 'quotes-collection')."\" class=\"button-secondary\" />";
+        $display .= "<input type=\"submit\" name=\"bulkactionsubmit\" value=\"Aplicar\" class=\"button-secondary\" />";
         $display .= "&nbsp;&nbsp;&nbsp;";
-        $display .= __('Sort by: ', 'quotes-collection');
-        $display .= "<select name=\"orderby\">";
-        $display .= "<option value=\"quote_id\"{$option_selected['quote_id']}>".__('Quote', 'quotes-collection')." ID</option>";
-        $display .= "<option value=\"quote\"{$option_selected['quote']}>".__('Quote', 'quotes-collection')."</option>";
-        $display .= "<option value=\"author\"{$option_selected['author']}>".__('Author', 'quotes-collection')."</option>";
-        $display .= "<option value=\"source\"{$option_selected['source']}>".__('Source', 'quotes-collection')."</option>";
-        $display .= "<option value=\"time_added\"{$option_selected['time_added']}>".__('Date added', 'quotes-collection')."</option>";
-        $display .= "<option value=\"time_updated\"{$option_selected['time_updated']}>".__('Date updated', 'quotes-collection')."</option>";
-        $display .= "<option value=\"public\"{$option_selected['public']}>".__('Visibility', 'quotes-collection')."</option>";
-        $display .= "</select>";
-        $display .= "<select name=\"order\"><option{$option_selected['ASC']}>ASC</option><option{$option_selected['DESC']}>DESC</option></select>";
-        $display .= "<input type=\"submit\" name=\"orderbysubmit\" value=\"".__('Go', 'quotes-collection')."\" class=\"button-secondary\" />";
         $display .= "</div>";
-        $display .= '<div class="tablenav-pages"><span class="displaying-num">'.sprintf(_n('%d quote', '%d quotes', $quotes_count, 'quotes-collection'), $quotes_count).'</span><span class="pagination-links">'. $page_nav. "</span></div>";
         $display .= "<div class=\"clear\"></div>";
         $display .= "</div>";
-
 
 
         $display .= "<table class=\"widefat\">";
@@ -311,11 +294,11 @@ function portaldirceu_planos_management() {
             <th>".__('Tags', 'quotes-collection')."</th>
             <th>".__('Public?', 'quotes-collection')."</th>
         </tr></thead>";
-        $display .= "<tbody id=\"the-list\">{$quotes_list}</tbody>";
+        $display .= "<tbody id=\"the-list\">{$plans_list}</tbody>";
         $display .= "</table>";
 
         $display .= "<div class=\"tablenav\">";
-        $display .= '<div class="tablenav-pages"><span class="displaying-num">'.sprintf(_n('%d quote', '%d quotes', $quotes_count, 'quotes-collection'), $quotes_count).'</span><span class="pagination-links">'. $page_nav. "</span></div>";
+        $display .= '<div class="tablenav-pages"><span class="displaying-num">'.sprintf(_n('%d quote', '%d quotes', $plans_count, 'quotes-collection'), $plans_count).'</span><span class="pagination-links">'. $page_nav. "</span></div>";
         $display .= "<div class=\"clear\"></div>";
         $display .= "</div>";
 
