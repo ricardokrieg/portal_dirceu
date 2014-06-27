c9f4e09192466924128cae668ea7c178ab1723d4
Ricardo Franco <ricardo.krieg@gmail.com>
2014-06-27 00:15:35 -0300

    footer

--- PortalDirceu/footer.php
+++ PortalDirceu/footer.php
@@ -1,4 +1,6 @@
-        <div class='footer-shadow'></div>
+        <div class='footer-shadow'>
+            <div class='logo-footer'></div>
+        </div>
 
         <footer class='footer'>
             <?php wp_nav_menu(array('theme_location' => 'rodape', 'menu_class' => 'list-inline')); ?>
