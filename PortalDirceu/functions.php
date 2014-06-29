e48c57053659694126b45a3b43ff8fc091f4f101
Ricardo Franco <ricardo.krieg@gmail.com>
2014-06-29 14:20:42 -0300

    up

--- PortalDirceu/functions.php
+++ PortalDirceu/functions.php
@@ -836,13 +836,23 @@ function portal_dirceu_register_required_plugins() {
             'force_deactivation' => false,
             'external_url'       => '',
         ),
+        array(
+            'name'               => 'Google Maps Widget',
+            'slug'               => 'google-maps-widget',
+            'source'             => get_stylesheet_directory() . '/plugins/google-maps-widget.zip',
+            'required'           => true,
+            'version'            => '',
+            'force_activation'   => true,
+            'force_deactivation' => false,
+            'external_url'       => '',
+        ),
     );
 
     $config = array(
         'default_path' => '',                      // Default absolute path to pre-packaged plugins.
         'menu'         => 'tgmpa-install-plugins', // Menu slug.
         'has_notices'  => true,                    // Show admin notices or not.
-        'dismissable'  => true,                    // If false, a user cannot dismiss the nag message.
+        'dismissable'  => false,                    // If false, a user cannot dismiss the nag message.
         'dismiss_msg'  => '',                      // If 'dismissable' is false, this message will be output at top of nag.
         'is_automatic' => true,                   // Automatically activate plugins after installation or not.
         'message'      => '',                      // Message to output right before the plugins table.
---
+++ PortalDirceu/page-templates/contato.php
@@ -0,0 +1,40 @@
+<?php // Template Name: Contato ?>
+<?php get_header(); ?>
+
+<div class='container'>
+    <section id='main'>
+        <?php while (have_posts()): the_post(); ?>
+            <section class='page'>
+                <div class='post-thumbnail'>
+                    <?php do_shortcode('[bgmp-map]'); ?>
+                </div>
+
+                <?php the_title('<div class="page-title-container"><h2>', '</h2></div>'); ?>
+
+                <section class='content'>
+                    <div class='container with-padding'>
+                        <?php if (is_search()): ?>
+                            <?php the_excerpt(); ?>
+                        <?php else: ?>
+                            <?php the_content(__('Leia Mais', 'portaldirceu')); ?>
+                            <?php
+                                wp_link_pages(array(
+                                    'before'      => "<div class='page-links'><span class='page-links-title'>" .__('Páginas:', 'portaldirceu' ). "</span>",
+                                    'after'       => "</div>",
+                                    'link_before' => "<span>",
+                                    'link_after'  => "</span>",
+                                ));
+                            ?>
+                        <?php endif; ?>
+
+                        <div class='edit-link'>
+                            <?php edit_post_link(__('Editar', 'portaldirceu'), "<span class='edit-link'>", "</span>"); ?>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        <?php endwhile; ?>
+    </section>
+</div>
+
+<?php get_footer();
