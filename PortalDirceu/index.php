13e11726d580abf99e5f75fdd211ca7538b910ea
Ricardo Franco <ricardo.krieg@gmail.com>
2014-06-30 09:23:18 -0300

    remove trash

--- PortalDirceu/index.php
+++ PortalDirceu/index.php
@@ -6,26 +6,15 @@
 
     <?php if (have_posts()): ?>
         <section id='main'>
-            <?#php dynamic_sidebar('sidebar-home'); ?>
-
             <?php while (have_posts()): the_post(); ?>
                 <?php get_template_part('content', get_post_format()); ?>
             <?php endwhile; ?>
-
-            <div id='nav-below' class='navigation'>
-                <div class='nav-previous'><?php previous_posts_link(); ?></div>
-                <div class='nav-next'><?php next_posts_link(); ?></div>
-            </div>
         </section>
     <?php else: ?>
         <section id='main'>
             <?php get_template_part('content', 'none'); ?>
         </section>
     <?php endif; ?>
-
-    <aside>
-        <?#php get_sidebar(); ?>
-    </aside>
 </div>
 
-<?#php get_footer();
+<?php get_footer();
--- PortalDirceu/page-templates/inicial.php
+++ PortalDirceu/page-templates/inicial.php
@@ -1,10 +1,38 @@
 <?php // Template Name: Inicial ?>
 <?php get_header(); ?>
 
-<section id='main' class='fullwidth'>
-    <?php while (have_posts()): the_post(); ?>
-        <?php the_content(); ?>
-    <?php endwhile; ?>
-</section>
+<div class='container'>
+    <section id='main'>
+        <?php while (have_posts()): the_post(); ?>
+            <section class='page'>
+                <?php portaldirceu_post_thumbnail(); ?>
+
+                <?php the_title('<div class="page-title-container"><h2>', '</h2></div>'); ?>
+
+                <section class='content'>
+                    <div class='container with-padding'>
+                        <?php if (is_search()): ?>
+                            <?php the_excerpt(); ?>
+                        <?php else: ?>
+                            <?php the_content(__('Leia Mais', 'portaldirceu')); ?>
+                            <?php
+                                wp_link_pages(array(
+                                    'before'      => "<div class='page-links'><span class='page-links-title'>" .__('Páginas:', 'portaldirceu' ). "</span>",
+                                    'after'       => "</div>",
+                                    'link_before' => "<span>",
+                                    'link_after'  => "</span>",
+                                ));
+                            ?>
+                        <?php endif; ?>
+
+                        <div class='edit-link'>
+                            <?php edit_post_link(__('Editar', 'portaldirceu'), "<span class='edit-link'>", "</span>"); ?>
+                        </div>
+                    </div>
+                </section>
+            </section>
+        <?php endwhile; ?>
+    </section>
+</div>
 
 <?php get_footer();
